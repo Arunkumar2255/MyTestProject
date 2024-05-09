f5b9818406a853733750624de8e8a0ef5af8a09d
Arunkumar2255 <148421914+Arunkumar2255@users.noreply.github.com>
2024-05-09 11:45:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,7 @@
 provider "aws" {
   region = "us-east-1"  # Replace with your desired AWS region
 }
-
-module "ec2_instance" {
-  source = "./ec2-instance"  # Path to your EC2 instance module
+resource "aws_instance" "linux" {
 
   ami           = "ami-0d13e3e640877b0b9"
   instance_type = "t2.micro"
